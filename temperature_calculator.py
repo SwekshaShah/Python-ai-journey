995a7f489a047e03dd4f5ebd29aa2e5666753b32
Sweksha Shah <swekshaa15@gmail.com>
2026-05-26 09:16:58 +0545

    temperature convertor

--- temperature_calculator.py
+++ temperature_calculator.py
@@ -1,5 +1,21 @@
-temp= float(input("Enter temperature:"))
-choice= input("Enter direction ie. f , k, c")
-choice1= choice.upper()
-temp1= temp*9/5+32 if choice1=="C INTO F" else temp*9/5+273.15 if choice1=="C INTO K" else (temp-32)*5/9 if choice1=="F INTO C" else temp
-print(f"{temp}C is {temp1:.2f} in {choice}.")
+choice_have= input("Enter direction in c or f or k:")
+temp= float(input("Enter temperature in celcius:"))
+choice_want= input("Enter direction in c or f or k:")
+choice1= choice_have.upper()
+choice2= choice_want.upper()
+if choice1=="C" and choice2=="F":
+  temp1=temp*9/5+32
+elif choice1=="C" and choice2=="K":
+  temp1=temp+273.15
+elif choice1=="F" and choice2=="C":
+  temp1= (temp-32)*5/9
+elif choice1=="F" and choice2=="K":
+  temp1= temp*9/5+273.15
+elif choice1=="K" and choice2=="C":
+  temp1= temp-273.15
+elif choice1=="K" and choice2=="F":
+  temp1= (temp-273.15)*9/5+32
+else:
+  print("Invalid Choice")
+
+print(f"{temp:.2f}{choice_have} = {temp1:.2f}{choice_want}")
